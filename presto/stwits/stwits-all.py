9b3dabc881c88d847d57f291f8717a58f7a1a0fb
Jiri <bromjiri@gmail.com>
2016-10-29 00:16:31 +0200

    stwits all

---
+++ presto/stwits/stwits-all.py
@@ -0,0 +1,90 @@
+import requests
+import json
+import logging
+import os
+import datetime
+
+def run_collect(company):
+
+    logger.info(company + " started")
+
+    # files and vars
+    today = datetime.date.today()
+    yesterday = today - datetime.timedelta(1)
+    two_days_ago = today - datetime.timedelta(2)
+    file_name = os.path.dirname(os.path.realpath(__file__)) + "/output/" + company + "/stwits-" + company + "-" + str(yesterday) + ".csv"
+    output = open(file_name, "a")
+
+    max_id = '99999999'
+
+
+    for j in range(0,10):
+
+        logger.info("processing: " + str(j) + " of 10")
+
+        url = 'https://api.stocktwits.com/api/2/streams/symbol/' + company + '.json?max=' + str(max_id)
+        req = requests.get(url).json()
+
+        for i in range(0,30):
+
+            created_at = req['messages'][i]['created_at']
+            text = req['messages'][i]['body']
+
+
+            # keep yesterday only
+            if str(created_at)[0:10] == str(two_days_ago):
+                return
+
+            logger.debug(created_at + ', ' + text)
+            output.write('"' + created_at + '","' + text + '"\n')
+
+        max_id = req['cursor']['max']
+        print('max_id: ' + str(max_id))
+
+    #print(json.dumps(req, indent=4, sort_keys=True))
+
+    return
+
+
+##################
+# start
+
+# Logging settings
+logFormatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s", datefmt='%Y%m%d-%H:%M')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# file
+log_file = os.path.dirname(os.path.realpath(__file__)) + "/log/stwits-all.log"
+fileHandler = logging.FileHandler(log_file)
+fileHandler.setFormatter(logFormatter)
+logger.addHandler(fileHandler)
+
+# console
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+logger.addHandler(consoleHandler)
+
+
+logger.info("starting " + os.path.basename(__file__))
+
+
+#####
+
+companies = ["msft", "ko", "mcd", "ssnlf", "nflx", "nke", "tsla"]
+#companies = ["msft"]
+
+
+for company in companies:
+    run_collect(company)
+    logger.info(company + " finished")
+
+
+logger.info("ending " + os.path.basename(__file__))
+
+
+# end
+###################
+
+
+
