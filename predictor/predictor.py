b341bda17beb9b73444fa3646e31e20e4a7768da
bromjiri <bromjiri@gmail.com>
2017-05-14 01:32:38 +0200

    predictor update

--- predictor/predictor.py
+++ predictor/predictor.py
@@ -2,6 +2,8 @@ import settings
 import random
 import nltk
 import csv
+import pandas as pd
+import numpy as np
 
 class Days:
 
@@ -24,54 +26,88 @@ class Days:
         features[2] = sent_dict[self.days[2]]
         return features
 
-def get_dict(input_file):
 
-    stock_dict = dict()
+# def get_dict(input_file):
+#
+#     stock_dict = dict()
+#
+#     with open(input_file, 'r') as input:
+#         reader = csv.reader(input, delimiter=',')
+#         prev = next(reader)[1]
+#         for row in reader:
+#             if row[1] > prev:
+#                 stock_dict[row[0]] = 4
+#             else:
+#                 stock_dict[row[0]] = 0
+#             prev = row[1]
+#     return stock_dict
 
-    with open(input_file, 'r') as input:
-        reader = csv.reader(input, delimiter=',')
-        prev = next(reader)[1]
-        for row in reader:
-            if row[1] > prev:
-                stock_dict[row[0]] = 4
-            else:
-                stock_dict[row[0]] = 0
-            prev = row[1]
-    return stock_dict
+def get_dicts(diff_df, method):
+    diff_df['SentBin'] = diff_df.apply(func, args=(method,), axis=1)
+    diff_df['StockBin'] = diff_df.apply(func, args=('Stock',), axis=1)
+    # print(diff_df)
+
+    sent_dict = diff_df['SentBin'].dropna().astype(int).to_dict()
+    stock_dict = diff_df['StockBin'].dropna().astype(int).to_dict()
+
+    return sent_dict, stock_dict
 
 
-subject = "coca-cola"
-source = "twitter"
-conf_limit = "1.0"
+def func(row, col):
+    if pd.isnull(row[col]):
+        return row[col]
+    elif row[col] > 0:
+        return 4
+    else:
+        return 0
 
-input_file = settings.PREDICTOR_STOCK + "/" + subject + ".csv"
-stock_dict = get_dict(input_file)
 
-input_file = settings.PREDICTOR_SENTIMENT + "/" + source + "/" + source + "-sent-" + subject + "-" + conf_limit + ".csv"
-sent_dict = get_dict(input_file)
+def run_one(source, subject, precision, method):
+    input_file_path = settings.PREDICTOR_DIFF + '/' + source + '/' + subject + '/' + source + '-diff-' + subject + '-' + precision + '.csv'
+    # stock_dict = get_dict(input_file)
+    diff_df = pd.read_csv(input_file_path, sep=',', index_col='Date')
 
-days = dict()
-days = Days([x for x in sorted(stock_dict)[0:3]])
+    sent_dict, stock_dict = get_dicts(diff_df, method)
 
-# for key in sorted(stock_dict):
-#     print(key, stock_dict[key], sent_dict[key])
+    days = Days([x for x in sorted(stock_dict)[0:3]])
 
-features_list = list()
-for key in sorted(stock_dict)[2:]:
-    features = days.get_features(sent_dict)
-    # print(features)
-    features_list.append([features, stock_dict[key]])
-    days.shift(key)
-    # print(key, stock_dict[key], sent_dict[key])
+    # for key in sorted(stock_dict):
+    #     print(key, stock_dict[key], sent_dict[key])
 
-for f in features_list:
-    print(f)
+    features_list = list()
+    for key in sorted(stock_dict)[3:]:
+        features = days.get_features(sent_dict)
+        # print(features)
+        features_list.append([features, stock_dict[key]])
+        days.shift(key)
+        # print(key, stock_dict[key], sent_dict[key])
 
-random.shuffle(features_list)
+    for f in features_list:
+        print(f)
 
-training_set = features_list[:70]
-testing_set = features_list[70:]
+    random.shuffle(features_list)
+    # print(len(features_list))
 
-classifier = nltk.NaiveBayesClassifier.train(training_set)
-print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set)) * 100)
-classifier.show_most_informative_features(1)
+    training_set = features_list[:90]
+    testing_set = features_list[90:]
+
+    classifier = nltk.NaiveBayesClassifier.train(training_set)
+    print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set)) * 100)
+    classifier.show_most_informative_features(1)
+
+
+source = "stwits"
+subjects = ["microsoft"]
+# precisions = ["Sent0.6", "Sent0.8", "Sent1.0"]
+# method = ["Friday", "Sunday", "Weekend"]
+precisions = ["Sent0.6"]
+methods = ["Friday"]
+
+for precision in precisions:
+    for subject in subjects:
+        for method in methods:
+            print(source, subject, precision, method)
+            run_one(source, subject, precision, method)
+
+
+
---
+++ trainer/classifier_test_verbose.py
@@ -0,0 +1,209 @@
+import nltk
+import os
+import collections
+from nltk.classify import NaiveBayesClassifier
+from nltk.metrics import precision, recall, f_measure
+import datetime
+import pickle
+from statistics import mode
+
+import trainer.corpora as crp
+import trainer.features as ftr
+
+from nltk.classify import ClassifierI
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC, NuSVC
+
+
+class VoteClassifier(ClassifierI):
+    def __init__(self, *classifiers):
+        self._classifiers = classifiers
+
+    def classify(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+        # print(mode(votes))
+
+        if votes.count("pos") >= 3:
+            return "pos"
+        elif votes.count("neg") >= 3:
+            return "neg"
+        else:
+            return "neu"
+
+        # return mode(votes)
+
+    def confidence(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes / len(votes)
+        return conf
+
+
+def get_precision(trainfeats, testfeats, my_classifier, dataset, classifier_name):
+
+    dir = "output/" + dataset + "/algos2/"
+    os.makedirs(dir, exist_ok=True)
+
+    file_name = dir + dataset + "-" + classifier_name + ".csv"
+    output = open(file_name, 'a')
+    # output.write(str(datetime.datetime.today()) + "\n")
+
+    refsets = collections.defaultdict(set)
+    testsets = collections.defaultdict(set)
+
+    for i, (feats, label) in enumerate(testfeats):
+        refsets[label].add(i)
+        observed = my_classifier.classify(feats)
+        testsets[observed].add(i)
+
+    # precision and recall
+    accuracy = nltk.classify.util.accuracy(my_classifier, testfeats) * 100
+    pos_prec = precision(refsets['pos'], testsets['pos']) * 100
+    pos_rec = recall(refsets['pos'], testsets['pos']) * 100
+    neg_prec = precision(refsets['neg'], testsets['neg']) * 100
+    neg_rec = recall(refsets['neg'], testsets['neg']) * 100
+
+    # round
+    accuracy = round(accuracy, 1)
+    pos_prec = round(pos_prec, 1)
+    pos_rec = round(pos_rec, 1)
+    neg_prec = round(neg_prec, 1)
+    neg_rec = round(neg_rec, 1)
+
+    output_text = classifier_name + ", " + str(accuracy) + ", " + str(pos_prec) + ", " + str(neg_prec) + ", " + str(
+        pos_rec) + ", " + str(neg_rec) + "\n"
+
+    output.write(output_text)
+    output.flush()
+
+
+def train(trainfeats, testfeats, dataset, nlt = True, skl = True, most = 0):
+    # print('train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats)))
+
+    nltk_output = "none"
+    sklearn_output = "none"
+
+    if nlt:
+
+        my_classifier = NaiveBayesClassifier.train(trainfeats)
+        refsets = collections.defaultdict(set)
+        testsets = collections.defaultdict(set)
+
+        for i, (feats, label) in enumerate(testfeats):
+            refsets[label].add(i)
+            observed = my_classifier.classify(feats)
+            testsets[observed].add(i)
+
+        # precision and recall
+        accuracy = nltk.classify.util.accuracy(my_classifier, testfeats) * 100
+        pos_prec = precision(refsets['pos'], testsets['pos']) * 100
+        pos_rec = recall(refsets['pos'], testsets['pos']) * 100
+        neg_prec = precision(refsets['neg'], testsets['neg']) * 100
+        neg_rec = recall(refsets['neg'], testsets['neg']) * 100
+
+        # round
+        accuracy = round(accuracy, 1)
+        pos_prec = round(pos_prec, 1)
+        pos_rec = round(pos_rec, 1)
+        neg_prec = round(neg_prec, 1)
+        neg_rec = round(neg_rec, 1)
+
+        # print('pos F-measure:', f_measure(refsets['pos'], testsets['pos']))
+        # print('neg F-measure:', f_measure(refsets['neg'], testsets['neg']))
+        my_classifier.show_most_informative_features(most)
+
+        nltk_output = "nlt, " + str(accuracy) + ", " + str(pos_prec) + ", " + str(neg_prec) + ", " + str(
+            pos_rec) + ", " + str(neg_rec) + "\n"
+
+    if skl:
+
+        MNB_classifier = SklearnClassifier(MultinomialNB())
+        MNB_classifier._vectorizer.sort = False
+        my_classifier = MNB_classifier.train(trainfeats)
+        get_precision(trainfeats, testfeats, my_classifier, dataset, "mnb")
+
+        mnb = (nltk.classify.accuracy(MNB_classifier, testfeats)) * 100
+        mnb = round(mnb, 1)
+        print(mnb)
+
+        BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
+        BernoulliNB_classifier._vectorizer.sort = False
+        my_classifier = BernoulliNB_classifier.train(trainfeats)
+        get_precision(trainfeats, testfeats, my_classifier, dataset, "bnb")
+
+        bnb = (nltk.classify.accuracy(BernoulliNB_classifier, testfeats)) * 100
+        bnb = round(bnb, 1)
+        print(bnb)
+
+        LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+        LogisticRegression_classifier._vectorizer.sort = False
+        my_classifier = LogisticRegression_classifier.train(trainfeats)
+        get_precision(trainfeats, testfeats, my_classifier, dataset, "lr")
+
+        lr = (nltk.classify.accuracy(LogisticRegression_classifier, testfeats)) * 100
+        lr = round(lr, 1)
+        print(lr)
+
+        LinearSVC_classifier = SklearnClassifier(LinearSVC())
+        LinearSVC_classifier._vectorizer.sort = False
+        my_classifier = LinearSVC_classifier.train(trainfeats)
+        get_precision(trainfeats, testfeats, my_classifier, dataset, "lsvc")
+
+        lsvc = (nltk.classify.accuracy(LinearSVC_classifier, testfeats)) * 100
+        lsvc = round(lsvc, 1)
+        print(lsvc)
+
+        NuSVC_classifier = SklearnClassifier(NuSVC())
+        NuSVC_classifier._vectorizer.sort = False
+        my_classifier = NuSVC_classifier.train(trainfeats)
+        get_precision(trainfeats, testfeats, my_classifier, dataset, "nsvc")
+
+        nsvc = (nltk.classify.accuracy(NuSVC_classifier, testfeats)) * 100
+        nsvc = round(nsvc, 1)
+        print(nsvc)
+
+        voted_classifier = VoteClassifier(
+            NuSVC_classifier,
+            LinearSVC_classifier,
+            MNB_classifier,
+            BernoulliNB_classifier,
+            LogisticRegression_classifier)
+        get_precision(trainfeats, testfeats, voted_classifier, dataset, "voted")
+
+        voted = (nltk.classify.accuracy(voted_classifier, testfeats)) * 100
+        voted = round(voted, 1)
+        print(voted)
+
+        sklearn_output = "skl, " + str(mnb) + ", " + str(bnb) + ", " + str(lr) + ", " + str(lsvc) + ", " + str(nsvc) + ", " + str(voted) + "\n"
+
+
+    return (nltk_output, sklearn_output)
+
+
+
+if __name__ == '__main__':
+    COUNT = 5000
+    cut = int((COUNT/2)*4/5)
+
+    corpora = crp.Corpora("stwits", count=COUNT, shuffle=True)
+    features = ftr.Features(corpora, total=COUNT, stem="porter", bigram=True, stop=True, inf_count=-1, lower=True)
+    # features = ftr.Features(corpora, total=COUNT, bigram=True, stem="porter")
+
+    posfeats = features.get_features_pos()
+    negfeats = features.get_fearures_neg()
+
+    trainfeats = negfeats[:cut] + posfeats[:cut]
+    testfeats = negfeats[cut:] + posfeats[cut:]
+
+    print('train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats)))
+    nlt, skl = train(trainfeats, testfeats, skl=False, most=50)
+    print(nlt, skl)
